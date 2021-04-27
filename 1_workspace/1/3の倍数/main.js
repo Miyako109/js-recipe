1cb814ebffbefd28d0079a621a9142bb60812234
Miyako109 <snoopy385@icloud.com>
2021-04-28 07:37:33 +0900

    ^_^

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,15 +1,12 @@
 const FizzBuzz = function(number){
-  for( let n = 1; n <= number; n++){
-    if(n % 15 === 0){
-      console.log("FizzBuzz")
-    }else if (n % 5 === 0){
-      console.log("Buzz")
-    }else if (n % 3 === 0){
-      console.log("Fizz")
+  for(let n = 0; n <= number; n++){
+    if(n % 3 === 0 ||String(n).includes("3")){
+      console.log(n + "!!!!!")
     }else {
       console.log(n)
     }
   }
 } 
 
-FizzBuzz(75)
+
+FizzBuzz(100)
