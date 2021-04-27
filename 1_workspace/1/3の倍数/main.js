11ca45af93926931199a4f3259e222afb3c5362c
Miyako109 <snoopy385@icloud.com>
2021-04-28 07:04:50 +0900

    FizzBuzz

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,14 +1,15 @@
-const gennkiFunction = function(number) {
-  for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log(`${n}FizzBuzz`)
-    } else if (n % 5 === 0) {
-      console.log(n + "Buzz")
-    } else if (n % 3 === 0) {
-      console.log(n + "Fizz")
-    } else {
+const FizzBuzz = function(number){
+  for( let n = 1; n <= number; n++){
+    if(n % 15 === 0){
+      console.log("FizzBuzz")
+    }else if (n % 5 === 0){
+      console.log("Buzz")
+    }else if (n % 3 === 0){
+      console.log("Fizz")
+    }else {
       console.log(n)
     }
   }
-}
-gennkiFunction(30)
+} 
+
+FizzBuzz(75)
