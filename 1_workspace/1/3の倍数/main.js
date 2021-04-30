bed373adacd62fef1b2268ab1f0f00389fdc9671
Miyako109 <snoopy385@icloud.com>
2021-04-30 11:31:39 +0900

    三の倍数発展3

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,22 +1,27 @@
 const button = document.getElementById("button")
 
+//変数countを定義して、
+let count = 1
 
-//countの実行内容
-const count = function(number){
-for( let i = 1; i <= number; i++ ){
-  if (i % 3 === 0){
-    console.log(i + "!!!!!!!")
+const countUp = function(){
+
+  //その定義されたcountが3の倍数か否か確かめる
+  if(count % 3 === 0){
+    console.log(count + "!!!!!!")
   }else{
-    console.log(i)
+    console.log(count)
   }
-}
-}
+ //確かめたら、countを1増やす。
+  count += 1
 
+}
 //クリックしたら、intervalを使ってcountを実行
  
 
 button.onclick = function(){
-setInterval(count(24), 1000)
+setInterval(countUp, 1000)
+
+//countUpはcountの値を変えながら1秒ずつ実行される。
 }
 
 
