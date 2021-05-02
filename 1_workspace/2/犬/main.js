3849bf83691f2cff5a0cf67bc3f8d183a272a29e
Miyako109 <snoopy385@icloud.com>
2021-05-02 18:57:12 +0900

    自販機

--- 1_workspace/2/犬/main.js
+++ 1_workspace/2/犬/main.js
@@ -1,17 +1,19 @@
 const imageElement = document.getElementById("dog-image")
-const Button = document.getElementById("button")
+const button = document.getElementById("button")
+
+button.onclick = function(){
+  change()
+}
+
+
 // 指定したサーバーにデータを取りに行く
-
-Button.onclick = function(){
-
-
-  fetch("https://dog.ceo/api/breeds/image/random")
+const change = function(){
+  fetch("https://dog.ceo/api/breed/Akita/images/random")
   .then((res) => {
-    return res.json() // 結果を json として読み込む
+    return res.json() // 結果を json として読み込んで、次の then に渡す
   })
   .then((data) => {
     imageElement.src = data.message // 画像を表示する
   })
-
 }
 
